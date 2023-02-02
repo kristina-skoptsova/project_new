d16cfa59030f0e12984c065f87085b044a6608e5
kristina-skoptsova <82136398+kristina-skoptsova@users.noreply.github.com>
2023-02-02 14:25:52 +0300

    загрузка фото в профиль

--- django_auth/users/models.py
+++ django_auth/users/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='django_auth/media/default.jpg', upload_to='profile_pics')
+    image = models.ImageField(default='users/static/media/default.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
--- django_auth/users/views.py
+++ django_auth/users/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
+from django.shortcuts import render
+from .forms import ProfileUpdateForm
+from django.contrib import messages
 
 
 def home(request):
@@ -15,3 +18,22 @@ class SignUp(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+def profile(request):
+    if request.method == 'POST':
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if p_form.is_valid():
+            p_form.save()
+            messages.success(request, f'Ваш профиль успешно обновлен.')
+            return redirect('profile')
+
+    else:
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'p_form': p_form
+    }
+
+    return render(request, 'users/profile.html', context)
