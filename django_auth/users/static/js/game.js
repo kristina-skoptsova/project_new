1df7cd0d3086d516ca1a6dc13da28cb8d7ab9be3
kristina-skoptsova <82136398+kristina-skoptsova@users.noreply.github.com>
2023-02-21 13:47:37 +0300

    отображение картинок

--- django_auth/users/static/js/game.js
+++ django_auth/users/static/js/game.js
@@ -3,12 +3,23 @@ var ctx = cvs.getContext("2d");
 
 var bird = new Image();
 var bg = new Image(); // Создание объекта
-var fg = new Image(); 
+var fg = new Image();
 var pipeUp = new Image();
 var pipeBottom = new Image();
 
-bird.src = "img/bird.png"; // Указание нужного изображения
-bg.src = "img/bg.png";
-fg.src = "img/fg.png";
-pipeUp.src = "img/pipeUp.png";
-pipeBottom.src = "img/pipeBottom.png";
+bird.src = "static/img/bird.png"; // Указание нужного изображения
+bg.src = "static/img/bg.png";
+fg.src = "static/img/fg.png";
+pipeUp.src = "static/img/pipeUp.png";
+pipeBottom.src = "static/img/pipeBottom.png";
+
+var gap = 90;
+
+function draw() {
+    ctx.drawImage(bg, 0, 0);
+    ctx.drawImage(pipeUp, 100, 0);
+    ctx.drawImage(pipeBottom, 100, 0 + pipeUp.height + gap);
+    ctx.drawImage(fg, 0, cvs.height - fg.height);
+    ctx.drawImage(bird, 10, 150);
+}
+pipeBottom.onload = draw;
