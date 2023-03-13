ea60e1c8fc640540de13492e97be855fe4c31a22
kristina-skoptsova <82136398+kristina-skoptsova@users.noreply.github.com>
2023-03-13 14:10:04 +0300

    bird
    задание условий

--- django_auth/users/static/js/game.js
+++ django_auth/users/static/js/game.js
@@ -15,11 +15,59 @@ pipeBottom.src = "static/img/pipeBottom.png";
 
 var gap = 90;
 
+// При нажатии на кнопку
+document.addEventListener('keydown', moveUp);
+function moveUp() {
+    yPos -= 25;
+}
+
+//Создание блоков
+var pipe = [];
+pipe[0] = {
+    x: cvs.width,
+    y: 0
+}
+var score = 0;
+//позиция птицы
+var xPos = 5;
+var yPos = 150;
+var grav = 1.5;
+
+
 function draw() {
     ctx.drawImage(bg, 0, 0);
-    ctx.drawImage(pipeUp, 100, 0);
-    ctx.drawImage(pipeBottom, 100, 0 + pipeUp.height + gap);
+    for(var i=0; i < pipe.length; i++) {
+        ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
+        ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
+        pipe[i].x--;
+        //задаются рандомно блоки
+        if (pipe[i].x == 125) {
+            pipe.push({
+                x: cvs.width,
+                y: Math.floor(Math.random() * pipeUp.height) - pipeUp.height
+            });
+        }
+        // проверка прикосновений
+        if(xPos + bird.width >=pipe[i].x
+           && xPos <= pipe[i].x + pipeUp.width
+           && (yPos <= pipe[i].y + pipeUp.height || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height) {
+                location.reload(); //Заново начинается игра
+           }
+        if(pipe[i].x == 5) {
+            score++;
+        }
+    }
+
+
     ctx.drawImage(fg, 0, cvs.height - fg.height);
-    ctx.drawImage(bird, 10, 150);
+    ctx.drawImage(bird, xPos, yPos);
+    yPos += grav;
+
+    ctx.fillStyle = '#000';
+    ctx.font = '24px Verdana';
+    ctx.fillText('Счет: ' + score, 10, cvs.height - 20)
+
+    requestAnimationFrame(draw)
 }
 pipeBottom.onload = draw;
+
